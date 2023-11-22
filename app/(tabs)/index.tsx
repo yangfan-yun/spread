0209a06dac0612ab083b10c40b9f94f3ad99734f
acui51 <acui@stanford.edu>
2023-11-21 21:45:41 -0800

    F

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,36 @@
-import { StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
-import React from 'react';
-import { Tab, TabBar } from '@ui-kitten/components';
+import React from "react";
+import { Layout, Tab, TabBar } from "@ui-kitten/components";
 
 export const TabBarSimpleUsageShowcase = (): React.ReactElement => {
-
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   return (
-    <TabBar
-      selectedIndex={selectedIndex}
-      onSelect={index => setSelectedIndex(index)}
-    >
-      <Tab title="Bets I'm in" />
-      <Tab title="Bets I'm judging" />
-    </TabBar>
+    <SafeAreaView style={styles.container}>
+      <Layout style={{ flex: 1 }}>
+        <TabBar
+          selectedIndex={selectedIndex}
+          onSelect={(index) => setSelectedIndex(index)}
+        >
+          <Tab title="Bets I'm in" />
+          <Tab title="Bets I'm judging" />
+        </TabBar>
+      </Layout>
+    </SafeAreaView>
   );
 };
 
 export default function BetsBets() {
-  return (
-      <TabBarSimpleUsageShowcase />
-  );
+  return <TabBarSimpleUsageShowcase />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
